baae8812d202650e7b645087e22e864ec39e4707
NordinP22R <49874265+nordinmp@users.noreply.github.com>
2024-01-04 13:24:44 +0100

    pin and delete stories

--- lib/api/notification_service.dart
+++ lib/api/notification_service.dart
@@ -5,35 +5,66 @@ import 'package:intl/intl.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
+import 'dart:math';
 
-tz.TZDateTime time = tz.TZDateTime.now(tz.local);
-String formattedTime = DateFormat('yyyy-MM-dd – kk:mm:ss:SSS').format(time);
+
+Random random = Random();
+int randomHour = random.nextInt(23);
+int randomMinute = random.nextInt(60);
+Duration randomDuration = Duration(hours: randomHour, minutes: randomMinute);
 
 
 class NotificationService {
-  void selectNotification(NotificationResponse response) {
-  // Update the time when the notification was sent
-  DateTime notificationSentTime = DateTime.now();
+  tz.TZDateTime startOfDay = tz.TZDateTime.now(tz.local).subtract(Duration(hours: tz.TZDateTime.now(tz.local).hour, minutes: tz.TZDateTime.now(tz.local).minute));
 
-  // Calculate the time left in TZDateTime format
-  tz.TZDateTime timeLeftInTz = time.add(const Duration(minutes:3));
+  tz.TZDateTime get randomTime => startOfDay.add(randomDuration);
 
-  // Calculate the difference between the current time and the notification sent time
-  Duration difference = timeLeftInTz.difference(notificationSentTime);
-  int seconds = difference.inSeconds; 
+  String get formattedTime => DateFormat('yyyy-MMMM-dd – kk:mm:ss:SSS').format(randomTime);
 
-  // Handle notification tap
-  navigatorKey.currentState?.pushNamed(
-    'camera',
-    arguments: {
-      'isTime': true,
-      'timeLeft': seconds,
-    },
-  );
-  }
+  tz.TZDateTime? updatedRandomTime;
 
   // Singleton pattern
   static final NotificationService _notificationService = NotificationService._internal();
+  static NotificationService get instance => _notificationService;
+
+  void checkAndUpdateTime() {
+    // Check if randomTime is in the past
+    if (randomTime.isBefore(DateTime.now())) {
+      // If it is, add one day to it
+      updatedRandomTime = randomTime.add(Duration(days: 1));
+    }
+  }
+
+  DateTime? lastNotificationDate;
+  void selectNotification(NotificationResponse response) {
+
+    if (response.payload == 'scheduled') {
+      // Update the time when the notification was sent
+      DateTime notificationSentTime = DateTime.now();
+
+      // Calculate the time left in TZDateTime format
+      tz.TZDateTime timeLeftInTz = randomTime.add(const Duration(minutes:3));
+
+      // Calculate the difference between the current time and the notification sent time
+      Duration difference = timeLeftInTz.difference(notificationSentTime);
+      int seconds = difference.inSeconds;
+
+      // Handle notification tap
+      navigatorKey.currentState?.pushNamed(
+        'camera',
+        arguments: {
+          'isTime': true,
+          'scheduledTime': seconds,
+        },
+      );
+
+      scheduleNotifications(); //Schedule new notification
+    } else {
+      print('Something else');
+    }
+  }
+
+  // Singleton pattern
   factory NotificationService() {
     return _notificationService;
   }
@@ -65,6 +96,8 @@ class NotificationService {
       initializationSettings,
       onDidReceiveNotificationResponse: selectNotification,
     );
+
+    checkAndUpdateTime(); // check om tiden er gået
   }
 
   static const AndroidNotificationDetails androidPlatformChannelSpecifics =
@@ -81,10 +114,14 @@ class NotificationService {
   late NotificationDetails instancePlatformChannelSpecifics;
 
   NotificationService._internal() {
+    updatedRandomTime = randomTime;
     instancePlatformChannelSpecifics = const NotificationDetails(
       android: androidPlatformChannelSpecifics,
       iOS: iOSPlatformChannelSpecifics
     );
+  }
+  bool isSameDay(DateTime date1, DateTime date2) {
+    return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day;
   }
 
   int getNotificationId() {
@@ -93,28 +130,34 @@ class NotificationService {
   }
 
   Future<void> scheduleNotifications() async {
-    try {
-      print('Scheduling notification...');
-      tz.TZDateTime time = tz.TZDateTime.now(tz.local).add(const Duration(seconds:20));
-      String formattedTime = DateFormat('yyyy-MM-dd – kk:mm:ss:SSS').format(time);
+  try {
+    print('Scheduling notification...');
+    DateTime now = DateTime.now();
+    if (lastNotificationDate == null || !isSameDay(lastNotificationDate!, now)) {
+
 
       await flutterLocalNotificationsPlugin.zonedSchedule(
         getNotificationId(),
-        formattedTime,
-        "This is the Notification Body!",
-        time,
-        instancePlatformChannelSpecifics,
+        '❗️Story time❗️',
+        "It's time to take a Story",
+        updatedRandomTime!,
+        instancePlatformChannelSpecifics,  
+        payload: 'scheduled',
         androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
         uiLocalNotificationDateInterpretation:
             UILocalNotificationDateInterpretation.absoluteTime,
       );
       print('Notification scheduled successfully.');
-
-      var pendingNotifications = await flutterLocalNotificationsPlugin.pendingNotificationRequests();
-      print('Number of pending notifications: ${pendingNotifications.length}');
-    } catch (e, stacktrace) {
-      print('Error scheduling notification: $e\n$stacktrace');
+      lastNotificationDate = now;
+    } else {
+      print('Notification already sent today.');
     }
+
+    var pendingNotifications = await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+    print('Number of pending notifications: ${pendingNotifications.length}');
+  } catch (e, stacktrace) {
+    print('Error scheduling notification: $e\n$stacktrace');
+  }
   }
 
   Future<void> sendNotificationNow() async {
@@ -146,5 +189,6 @@ class NotificationService {
 
   Future<void> cancelAllNotifications() async {
     await flutterLocalNotificationsPlugin.cancelAll();
+    lastNotificationDate = null;
   }
 }
--- lib/pages/stories_page.dart
+++ lib/pages/stories_page.dart
@@ -18,6 +18,7 @@ class StoriesPage extends StatefulWidget
 }
 class _StoriesPage extends State<StoriesPage>
 {
+  late List<Map<String, dynamic>> storiesData;
 
   Stream<Map<String, List<Map<String, dynamic>>>> _getPhotosAndStoriesStream(String userId) {
     // Fetch all 'story' from the 'photos' collection where 'isFavorite' is true
@@ -59,6 +60,91 @@ class _StoriesPage extends State<StoriesPage>
           });
     }
 
+
+  Future<void> updateDefaultStory() async {
+    // Get a reference to the Firestore instance
+    final firestore = FirebaseFirestore.instance;
+
+    // Get a reference to the 'users' collection
+    final usersCollection = firestore.collection('users');
+
+    // Get a reference to the 'stories' collection of the current user
+    final storiesCollection = usersCollection.doc(user['userId']).collection('stories');
+
+    // Fetch all stories where 'default' is true
+    final defaultStoriesSnapshot = await storiesCollection.where('default', isEqualTo: true).get();
+
+    // Create a batch to perform multiple operations in one go
+    final batch = firestore.batch();
+
+    // For each 'default' story, update 'default' to false
+    for (final doc in defaultStoriesSnapshot.docs) {
+      batch.update(doc.reference, {'default': false});
+    }
+
+    // Update 'default' field of the current story to true
+    batch.update(storiesCollection.doc(storiesData[0]['id']), {'default': true});
+
+    // Commit the batch
+    await batch.commit();
+
+    // Trigger a rebuild of the widget
+    setState(() {});
+  }
+
+  Future<void> deleteStory() async {
+  // Show a dialog asking the user to confirm the deletion
+  final bool? confirm = await showDialog<bool>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Confirm Deletion'),
+        content: const Text('Are you sure you want to delete this story?'),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Cancel'),
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+          ),
+          TextButton(
+            child: const Text('Yes'),
+            onPressed: () {
+              Navigator.of(context).pop(true);
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+        ],
+      );
+    },
+  );
+
+  // If the user confirmed the deletion and the story id is not empty, delete the story
+  if (confirm == true && widget.storyId != '') {
+    // Get a reference to the Firestore instance
+    final firestore = FirebaseFirestore.instance;
+
+    // Get a reference to the 'users' collection
+    final usersCollection = firestore.collection('users');
+
+    // Get a reference to the 'stories' collection of the current user
+    final storiesCollection = usersCollection.doc(user['userId']).collection('stories');
+
+    // Delete the story
+    await storiesCollection.doc(widget.storyId).delete();
+
+    // Show a SnackBar
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Story deleted'),
+      ),
+    );
+
+    // Trigger a rebuild of the widget
+    setState(() {});
+  }
+}
+  
   @override
   Widget build(BuildContext context) {
 
@@ -66,66 +152,71 @@ class _StoriesPage extends State<StoriesPage>
     double width = fullWidth * 0.9;
 
     String titleText = "Stories";
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-              titleText,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.scrim,
+    return StreamBuilder<Map<String, List<Map<String, dynamic>>>>(
+      stream: _getPhotosAndStoriesStream(user['userId']),
+      builder: (BuildContext context, AsyncSnapshot<Map<String, List<Map<String, dynamic>>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(body: LoadingAnimation());
+        } else if (snapshot.hasError) {
+          return Scaffold(body: Center(child: Text('Error: ${snapshot.error}')));
+        } else if (snapshot.hasData) {
+          storiesData = snapshot.data!['stories']!;
+          List<Map<String, dynamic>> photosData = snapshot.data!['photos']!;
+
+          print('Stories: $storiesData');
+          print('Photos: $photosData');
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                titleText,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.scrim,
+                ),
+              ),
+              actions: [
+                IconButton(
+                  onPressed: storiesData.isNotEmpty ? updateDefaultStory : null,
+                  icon: storiesData.isNotEmpty && storiesData[0]['default'] 
+                    ? const Icon(Icons.push_pin) 
+                    : const Icon(Icons.push_pin_outlined),
+                ),
+                IconButton(
+                  onPressed: deleteStory ,
+                  icon: const Icon(Icons.delete_outline),
+                )
+              ],
+            ),
+            body: Center(
+              child: SizedBox(
+                width: width,
+                child: photosData.isEmpty
+                ? const EmptyState()
+                : ListView.separated(
+                    itemCount: photosData.length,
+                    separatorBuilder: (BuildContext context, int index) => const Gap(10),
+                    itemBuilder: (BuildContext context, int index) {
+                      Map<String, dynamic> storyImageData = photosData[index];
+                      Map<String, dynamic> storyStoriesData = storiesData[index];
+                      DateTime dateTaken = (storyImageData['dateTaken'] as Timestamp).toDate();
+
+                      return StoryAsset(
+                        imagePath: storyImageData['imagePath'],
+                        isFavorite: storyImageData['isFavorite'],
+                        imageDate: dateTaken,
+                        storyPath: storyStoriesData['imagePath'],
+                        storyTitle: storyStoriesData['title'],
+                        imageId: storyImageData['id'],
+                      );
+                    },
+                  ),
               ),
             ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-              child: StreamBuilder<Map<String, List<Map<String, dynamic>>>>(
-                stream: _getPhotosAndStoriesStream(user['userId']),
-                builder: (BuildContext context, AsyncSnapshot<Map<String, List<Map<String, dynamic>>>> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const LoadingAnimation();
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else if (snapshot.hasData) {
-                    List<Map<String, dynamic>> storiesData = snapshot.data!['stories']!;
-                    List<Map<String, dynamic>> photosData = snapshot.data!['photos']!;
-
-                    print('Stories: $storiesData');
-                    print('Photos: $photosData');
-
-                    return Center(
-                      child: SizedBox(
-                        width: width,
-                        child: photosData.isEmpty
-                        ? const EmptyState()
-                        : ListView.separated(
-                            itemCount: photosData.length,
-                            separatorBuilder: (BuildContext context, int index) => const Gap(10),
-                            itemBuilder: (BuildContext context, int index) {
-                              Map<String, dynamic> storyImageData = photosData[index];
-                              Map<String, dynamic> storyStoriesData = storiesData[index];
-                              DateTime dateTaken = (storyImageData['dateTaken'] as Timestamp).toDate();
-                          
-                              return StoryAsset(
-                                imagePath: storyImageData['imagePath'],
-                                isFavorite: storyImageData['isFavorite'],
-                                imageDate: dateTaken,
-                                storyPath: storyStoriesData['imagePath'],
-                                storyTitle: storyStoriesData['title'],
-                                imageId: storyImageData['id'],
-                              );
-                            },
-                          ),
-                      ),
-                    );
-                  } else {
-                    return const LoadingAnimation();
-                  }
-                },
-              ),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return const Scaffold(body: LoadingAnimation());
+        }
+      },
     );
   }
 }
